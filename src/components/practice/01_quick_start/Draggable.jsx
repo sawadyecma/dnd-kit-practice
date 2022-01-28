b9a4b7010a1f24d1a14f192c70a22f62ecd6dd0d
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-28 19:37:37 +0900

    use @dnd-kit/utilities

--- src/components/practice/01_quick_start/Draggable.jsx
+++ src/components/practice/01_quick_start/Draggable.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { useDraggable } from '@dnd-kit/core'
+import { CSS } from '@dnd-kit/utilities'
 
 export function Draggable(props) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: 'draggable',
   })
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-      }
-    : undefined
+
+  // const style = transform
+  //   ? {
+  //       transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  //     }
+  //   : undefined
+
+  // Within your component that receives `transform` from `useDraggable`:
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  }
 
   return (
     <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
